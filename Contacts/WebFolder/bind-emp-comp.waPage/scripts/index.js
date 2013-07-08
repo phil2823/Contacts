295107ec80156bfaed430d71cc408f3d67ecb6f7
phil2823 <philip2823@gmail.com>
2013-07-08 15:09:19 -0400

    Initial commit

---
+++ Contacts/WebFolder/bind-emp-comp.waPage/scripts/index.js
@@ -0,0 +1,23 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		
+// Create the association
+//set the company field in employee to the currently seleted company
+    sources.employee.company.set( sources.company ); 
+// Save the employee
+    sources.employee.save();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
